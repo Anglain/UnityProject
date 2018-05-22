b2efbaf7b10a04d859d107357a3b53f90e60fd66
Anglain <shudra.ia@gmail.com>
2018-05-22 11:22:01 +0300

    Added player, edge platform, four animations and animation controller

---
+++ Assets/Scripts/HeroFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour
+{
+	private HeroRabit player;
+
+	void Start ()
+	{
+		player = GameObject.FindWithTag("Player").GetComponent<HeroRabit>();
+
+		if (player == null)
+		{
+			Debug.LogError("No gameObject with the HeroRabit component found in the scene! [HERO_FOLLOW.CS]");
+		}
+	}
+
+	void Update ()
+	{
+		Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
---
+++ Assets/Scripts/HeroRabit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabit : MonoBehaviour
+{
+	public float speed = 30f;
+	public float deathOffset;
+
+	private float movingAxisValue;
+	private Rigidbody2D rb2D;
+	private SpriteRenderer sr;
+	private Animator anim;
+
+	void Start ()
+	{
+		rb2D = this.GetComponent<Rigidbody2D>();
+		sr = this.GetComponent<SpriteRenderer>();
+		anim = this.GetComponent<Animator>();
+
+		if (rb2D == null) Debug.LogError("No Rigidbody2D component found attached to this Player gameObject! [HERO_RABIT.CS]");
+		if (sr == null)	Debug.LogError("No SpriteRenderer component found attached to this Player gameObject! [HERO_RABIT.CS]");
+		if (anim == null) Debug.LogError("No Animator component found attached to this Player gameObject! [HERO_RABIT.CS]");
+	}
+
+	void FixedUpdate ()
+	{
+		movingAxisValue = Input.GetAxis("Horizontal");
+
+		if (Mathf.Abs(movingAxisValue) > 0)
+		{
+			rb2D.velocity = new Vector2(movingAxisValue * speed * Time.deltaTime * 10f, rb2D.velocity.y);
+		}
+
+		if (movingAxisValue != 0)
+		{
+			sr.flipX = (movingAxisValue < 0 ? true : false);
+		}
+
+		anim.SetBool("isMoving", (Mathf.Abs(rb2D.velocity.x) < 0.05f));
+		anim.SetBool("isGrounded", (Mathf.Abs(rb2D.velocity.y) < 0.05f));
+
+		if (transform.position.y < deathOffset) anim.SetTrigger("isDead");
+	}
+}
